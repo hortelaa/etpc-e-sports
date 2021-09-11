3adb5a4c32ee0b3ebccf6d052afedb6309c1a178
hortelaa <lucascardoso5263@gmail.com>
2021-09-11 20:59:57 -0300

    Fundo tela de cadastro

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'fundo': "url('/src/imgs/fundocadastro.png')",
+       }
+    },
   },
   variants: {
     extend: {},
