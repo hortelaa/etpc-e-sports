33a7d2523039329fdbb8b264f033b744d455a591
hortelaa <lucascardoso5263@gmail.com>
2021-11-04 17:48:26 -0300

    Co-authored-by: José Miguel <GuelBandeira@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily:{
+        'denis': ['Bai Jamjuree', 'sans-serif']
+      },
       colors:{
         'camp-claro':'#510cc7',
         'camp-escuro':'#8c2cda',
