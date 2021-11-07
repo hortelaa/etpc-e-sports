9c0bbd1af2e77ed9fbc56f40cb69aed819b26dd1
GuelBandeira <bandeiraguel@gmail.com>
2021-11-07 11:27:40 -0300

    Implementação do Banco e Ajustes Final no Cadastro

--- src/assets/js/cadastro-termos.js
+++ src/assets/js/cadastro-termos.js
@@ -1,6 +1,9 @@
 
     var botao = $('#continuar');
 
+    botao.click(function() {
+        window.location.replace("cadastro-confirmacao.html");
+     });    
  
     
     let scroll = document.getElementById('overflow');
@@ -11,9 +14,12 @@
                 botao.attr("disabled", false);
                 botao.removeClass('cursor-not-allowed');
                 botao.removeClass('bg-opacity-50');
-                botao.addClass('border-purple-800');
-                botao.addClass('transition duration-500 ease-out transform hover:scale-105 hover:bg-purple-800');
+                botao.addClass('border-purple-600');
+                botao.addClass('bg-opacity-80');
+                botao.addClass('highlight');
+                botao.addClass('transition duration-500 ease-out transform hover:scale-105 hover:bg-purple-600 hover:border-purple-800');
             }
         
         }
-    
+
+
--- src/assets/php/cadastro.php
+++ src/assets/php/cadastro.php
@@ -7,20 +7,34 @@ $nome = $_POST['nome'];
 $turma_e_curso = $_POST['turma-e-curso'];
 $nicks = $_POST['nick-dos-integrantes-da-equipe'];
 
+
+
+
+$conexao = new mysqli('localhost','root','root','etpc-esports');
+if ($conexao->connect_error){
+    die();
+    echo "<meta http-equiv='refresh' content='0;URL=../../../public/cadastro-erro.html'>";
+    }else{
+        $stmt = $conexao->prepare("INSERT Into registro(email, equipe, matricula, nome, turma_e_curso, nicks) values(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssisss",$email, $equipe, $matricula, $nome, $turma_e_curso, $nicks);
+        $stmt->execute();
+        echo "<meta http-equiv='refresh' content='0;URL=../../../public/cadastro-termos.html'>";
+        $stmt->close();
+        $conexao->close();
+}
+//<================================METÓDO-ANTIGO================================>
 //Compo E-mail
-$conteudo = "
- <=====EQUIPE=====>
- Email: $email
- Nome Completo: $nome
- Nome da Equipe: $equipe
- Matrícula: $matricula
- Turma e curso: $turma_e_curso
- Nick's: $nicks\n
-";
-
-$arquivo = fopen("info.txt" , "a+");
-fwrite($arquivo, $conteudo);
-
-echo "<meta http-equiv='refresh' content='0;URL=../../../public/cadastro-termos.html'>";
+//$conteudo = "
+// <=====EQUIPE=====>
+// Email: $email
+// Nome Completo: $nome
+// Nome da Equipe: $equipe
+// Matrícula: $matricula
+//Turma e curso: $turma_e_curso
+//Nick's: $nicks\n
+//";
+//$arquivo = fopen("info.txt" , "a+");
+//fwrite($arquivo, $conteudo);
+//echo "<meta http-equiv='refresh' content='0;URL=../../../public/cadastro-termos.html'>";
 
 ?>
